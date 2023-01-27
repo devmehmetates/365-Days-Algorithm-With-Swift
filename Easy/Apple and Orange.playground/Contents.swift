62d279e70741a82ee4bdbd8921b4021d1abad83b
Mehmet Ateş  <Mehmet1095@icloud.com>
2023-01-27 19:38:15 +0300

    Apples and oranges completed

---
+++ Easy/Apple and Orange.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+func countApplesAndOranges(s: Int, t: Int, a: Int, b: Int, apples: [Int], oranges: [Int]) -> Void {
+    print(apples.map { a + $0 }.filter { s...t ~= $0 }.count)
+    print(oranges.map { b + $0 }.filter { s...t ~= $0 }.count)
+}
+
+countApplesAndOranges(s: 2, t: 3, a: 1, b: 5, apples: [2], oranges: [-2])
