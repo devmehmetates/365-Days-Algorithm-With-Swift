4364233c6962c8fe3cc61c6bbb08dd97f13983d5
Mehmet Ateş  <Mehmet1095@icloud.com>
2023-01-27 19:38:55 +0300

    Update Contents.swift

--- Easy/Apple and Orange.playground/Contents.swift
+++ Easy/Apple and Orange.playground/Contents.swift
@@ -1,5 +1,7 @@
 import UIKit
 
+/// URL
+/// https://www.hackerrank.com/challenges/apple-and-orange/problem
 func countApplesAndOranges(s: Int, t: Int, a: Int, b: Int, apples: [Int], oranges: [Int]) -> Void {
     print(apples.map { a + $0 }.filter { s...t ~= $0 }.count)
     print(oranges.map { b + $0 }.filter { s...t ~= $0 }.count)
